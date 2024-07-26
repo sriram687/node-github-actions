68ca2b2b05079a85a7d97b724cf5382525df4ec8
pikachu <msram2006@gmail.com>
2024-07-26 17:14:25 +0530

    update 1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with github actions and github pages
         </a>
       </header>
     </div>
